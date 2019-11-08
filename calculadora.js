ad3fb989de31a493e47cdc5694cb927a615d6244
gabriel <gabrieldejesus-21@hotmail.com>
2019-11-08 20:47:02 -0300

    primeiro commit

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+//declare dias variaveis que recebam numeros inteiros
+//utilizando o console.log()
+//exiba o resultado das 4 operações básicas(+-*/)
+
+numeroOne=04;
+numeroTwo=04;
+
+somaTotal=numeroOne+numeroTwo;
+
+divisaoTotal=numeroOne/numeroTwo;
+
+subtrairTotal=numeroOne-numeroTwo;
+
+multiplicacaoTotal=numeroOne*numeroTwo;
+
+
+console.log("somaTotal:" + somaTotal);
+
+console.log("divisaoTotal:" + divisaoTotal);
+
+console.log("subtrairTotal:" + subtrairTotal);
+
+console.log("multiplicacaoTotal:"  + multiplicacaoTotal);
---
+++ idade.js
@@ -0,0 +1,15 @@
+//criar uma variavel que declare idade
+//caso a idade seja menor que 16 informar:
+
+
+ideda:15;
+
+    if(idade <16) 
+    {
+        console.log("nenem nao pode entrar"...)
+    }
+
+    else if(idade <18) 
+    {
+        console.log(" entar permitida com os pais")
+    }
